57475b5c5264fb48f2bfbdad275c0e5214d38655
alruiz95 <alruiz300795@gmail.com>
2017-11-13 22:31:51 -0600

    FINAL

--- Redes/UDP/client.py
+++ Redes/UDP/client.py
@@ -1,61 +1,117 @@
 import socket
+import pip
 import tkinter as tk
 from tkinter import filedialog
 import time
+import os
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip = "172.24.40.22"
+print("Cual es la direccion ip?")
+ip = input()
 port = 1234
-address = (ip, port)
+address_G = (ip,port)
 buffer = 1024
 
+def encode_length(l):
 
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
-file_name = file_path.split("/")[-1]
-client.sendto("sendFile".encode("utf-8", "strict"), address)
-client.sendto(file_name.encode("utf-8", "strict"), address)
-file = open(file_path, "rb")
-data = file.read(buffer)
-print("Sending file")
-while data:
-    if client.sendto(data, address):
-        print("   loading...")
-        data = file.read(buffer)
-file.close()
-print("   Done")
+    #Make it 4 bytes long
+    l = str(l)
+    while len(l) < 4:
+        l = "0"+l
+    return l
 
-print("\n")
-time.sleep(3)
 
-client.sendto("receiveFilenames".encode("utf-8", "strict"), address)
-data, address = client.recvfrom(buffer)
-data = data.decode("utf-8", "strict")
-print("Reading files on the server:")
-for filename in data.split(","):
-    print("   " + filename)
+def menu():
+    print("=================================")
+    print("Consultas:")
+    print("1.Nombre del host")
+    print("2.Ip del servidor")
+    print("3.Cantidad de procesos ejecutandose")
+    print("4.Subir archivo al servidor")
+    print("5.Archivos del servidor")
+    print("6.Hora de un país")
+    print("7.Apagar Servidor")
+    print("0.Salir")
+    seleccion = int(input())
+    print("=================================")
 
-print("\n")
-time.sleep(2)
+    #==================================================== CONSULTA 1
+    if(seleccion == 1):
+        client.sendto(b"nombreHost", address_G)
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
 
-client.sendto("hostname".encode("utf-8", "strict"), address)
-data, address = client.recvfrom(buffer)
-print("Hostname: ", data.decode("utf-8", "strict"))
+    # ==================================================== CONSULTA 2
+    elif(seleccion == 2):
+        client.sendto(b"ipServe", address_G)
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
 
-print("\n")
-time.sleep(2)
+    # ==================================================== CONSULTA 3
+    elif(seleccion == 3):
+        client.sendto(b"cantPro", address_G)
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
 
-client.sendto("ipAddress".encode("utf-8", "strict"), address)
-data, address = client.recvfrom(buffer)
-print("IP address: ", data.decode("utf-8", "strict"))
+    # ==================================================== CONSULTA 4
+    elif(seleccion == 4):
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename()
+        file_name = file_path.split("/")[-1]
+        client.sendto(b"sendFile", address_G)
+        client.sendto(file_name.encode("utf-8", "strict"), address_G)
 
-print("\n")
-time.sleep(2)
 
-country = "COL"
-client.sendto(("timeAt " + country).encode("utf-8", "strict"), address)
-data, address = client.recvfrom(buffer)
-print("Current time in " + country + ": ", data.decode("utf-8", "strict"))
+        print("Enviando "+file_name)
+        time.sleep(1)
+        file = open(file_path, "rb")
+        print ('Sending...')
+        size = os.path.getsize(file_path)
+        client.sendto(str(size).encode("utf-8", "strict"),address_G)
+        time.sleep(1)
+        client.sendto(file.read(),address_G)
+        file.close()
+        print("Done Sending")
 
-client.close()
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
+
+    # ==================================================== CONSULTA 5
+    elif(seleccion == 5):
+        client.sendto(b"listArch", address_G)
+        print("====== LISTA de Archivos ========")
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
+        print("=================================")
+
+
+    # ==================================================== CONSULTA 6
+    elif(seleccion == 6):
+        print("Escriba el pais:")
+        pais = input()
+        enviar = "hora "+pais
+        client.sendto(enviar.encode("utf-8", "strict"), address_G)
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
+
+    elif(seleccion == 7):
+        client.sendto(b"shutdownServer", address_G)
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
+        client.close()
+        return
+
+    elif(seleccion == 0):
+        client.sendto(b"disconnect", address_G)
+        data, address = client.recvfrom(buffer)
+        print(data.decode("utf-8", "strict"))
+        client.close()
+        return
+    else:
+        print("Opcion Invalida")
+    menu()
+
+menu()
--- Redes/UDP/server.py
+++ Redes/UDP/server.py
@@ -1,6 +1,21 @@
 import socket
+import threading
+import pip
 import os
+import pathlib
+import sys
 
+
+def install(name):
+    pip.main(['install',name])
+
+install('geopy')
+install('pytz')
+
+from geopy import geocoders # $ pip  install geopy
+import calendar
+from datetime import datetime
+import pytz # $ pip install pytz
 
 def get_ip():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,48 +28,94 @@ def get_ip():
         sock.close()
     return server_ip
 
+def getTimePais(pais):
+    try:
+        g = geocoders.GoogleV3()
+        place, (lat, lng) = g.geocode(pais)
+        timezone = g.timezone((lat, lng))  # return pytz timezone object
+        print(timezone.zone)
+        now = datetime.now(pytz.timezone(timezone.zone))  # you could pass `timezone` object here
+        print (now)
+        return now, timezone.zone
+    except ValueError:
+        return "  Hora sin poder consultar" , "error"
+
 
 def listen_to_client():
     client_data, client_address = server.recvfrom(buffer)
     decoded_data = client_data.strip().decode("utf-8", "strict")
     print("   Received ", decoded_data, " from client")
     print("   Processing Data")
+
+
+
     if decoded_data == "sendFile":
+        client_data, client_address = server.recvfrom(buffer)
+        decoded_data = client_data.strip().decode("utf-8", "strict")
+        filename = decoded_data
+        print("   Filename: " + filename)
+
+        file = open('Files/' + filename, 'wb')
+
         print("   Receiving data")
         client_data, client_address = server.recvfrom(buffer)
         decoded_data = client_data.strip().decode("utf-8", "strict")
-        print("   Received File: " + decoded_data)
-        file = open("Files/" + decoded_data, 'wb')
-        client_data, client_address = server.recvfrom(buffer)
-        try:
-            while client_data:
-                file.write(client_data)
-                server.settimeout(2)
-                client_data, client_address = server.recvfrom(buffer)
-        except:
-            file.close()
-            print("   File Downloaded")
-    elif decoded_data == "hostname":
+        size = int(decoded_data)
+        print(size)
+        client_data, client_address = server.recvfrom(size)
+        decoded_data = client_data
+        data = decoded_data
+        print("   File Resived")
+        file.write(data)
+        file.close()
+        print("   File Downloaded")
+        server.sendto(b"Filed Resived", client_address)
+
+
+    elif decoded_data == "nombreHost":
         server.sendto(socket.gethostname().encode("utf-8", "strict"), client_address)
         print("   Hostname sent")
-    elif decoded_data == "ipAddress":
+
+
+    elif decoded_data == "ipServe":
         server.sendto(ip.encode("utf-8", "strict"), client_address)
         print("   IP address sent.")
-    elif decoded_data == "receiveFilenames":
+
+
+    elif decoded_data == "listArch":
         print("   Reading files saved on the server")
         result = ""
         for filename in os.listdir("Files/"):
             result += filename + ","
         server.sendto(result[:-1].encode("utf-8", "strict"), client_address)
         print("   Done, data sent")
-    elif decoded_data == "timeAt":
+
+
+    elif "hora " in decoded_data:
         print("   Getting time")
-        server.sendto("AQUI VA LA HORA CALCULADA".encode("utf-8", "strict"), client_address)
+        time, zone = getTimePais(decoded_data[5:])
+        if (zone == "error"):
+            server.sendto(b"ERROR:", client_address)
+            server.sendto(b"No se pudo cargar la hora", client_address)
+            print("  ERROR de hora")
+        else:
+            server.sendto(zone.encode("utf-8", "strict"), client_address)
+            server.sendto(str(time).encode("utf-8", "strict"), client_address)
+            print("  Hora Enviada")
         print("   Done, time sent")
+
+
+    elif decoded_data == "cantPro":
+        num_client = threading.activeCount() - 1
+        server.sendto(str(num_client).encode("utf-8", "strict"), client_address)
+        print("  Cantidad de Procesos enviados")
     else:
+        server.sendto(b"invalid data", client_address)
         print("   Done, invalid data")
     print("\n")
 
+
+pathlib.Path('Files').mkdir(parents=True, exist_ok=True)
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ip = get_ip()
 port = 1234
